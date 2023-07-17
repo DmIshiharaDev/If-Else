7bb6f03714bca4ccce7ca6437b977e788b9c977a
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-07-17 06:34:51 -0300

    Adicionar arquivos de projeto.

---
+++ If-Else/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("------ Dados de Pagamento ------");
+        Console.WriteLine();
+
+        Console.Write("Escreva seu nome: ");
+        string nome = Console.ReadLine();
+        Console.Write("Saldo em conta(INT): R$");
+        int saldo = int.Parse(Console.ReadLine());
+        Console.Write("Nome do produto");
+        string produto = Console.ReadLine();
+        Console.Write("Valor do produto(INT): R$");
+        int valorProduto = int.Parse(Console.ReadLine());
+
+        int conta = saldo - valorProduto;
+        bool aprovado = conta >= valorProduto; 
+        bool reprovado = conta <= valorProduto;
+
+        if (aprovado)
+        { 
+            Console.WriteLine("")
+        }
+   
+    }
+}
