e40870fe66c3271111820fb59ae8a32c5ccbc470
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-07-17 14:00:33 -0300

    Add files via upload
    Atualização do exercício de If /Else

--- If-Else/Program.cs
+++ If-Else/Program.cs
@@ -1,27 +1,45 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("------ Dados de Pagamento ------");
-        Console.WriteLine();
+﻿using System;
+using System.Globalization;
 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("------ Dados da compra ------");
+        Console.WriteLine();
         Console.Write("Escreva seu nome: ");
         string nome = Console.ReadLine();
-        Console.Write("Saldo em conta(INT): R$");
-        int saldo = int.Parse(Console.ReadLine());
-        Console.Write("Nome do produto");
-        string produto = Console.ReadLine();
-        Console.Write("Valor do produto(INT): R$");
-        int valorProduto = int.Parse(Console.ReadLine());
+        Console.Write("Saldo em conta: R$");
+        double saldo = double.Parse(Console.ReadLine());
+        Console.WriteLine();
 
-        int conta = saldo - valorProduto;
-        bool aprovado = conta >= valorProduto; 
-        bool reprovado = conta <= valorProduto;
+        Console.WriteLine("------ Dados do produto ------");
+        Console.WriteLine();
+        Console.Write("Nome do produto: ");
+        string produto = Console.ReadLine();
+        Console.Write("Valor do produto: R$");
+        double valorProduto = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        double conta = saldo - valorProduto;
+        bool aprovado = conta >= 0; 
+
+        Console.WriteLine("------ Dados do Pagamento ------");
+        Console.WriteLine();
 
         if (aprovado)
-        { 
-            Console.WriteLine("")
+        {
+            Console.WriteLine("Parabéns! Sua compra foi aprovada!");
+            Console.WriteLine("O valor do saldo restante é de: R$"+conta);
         }
-   
+        else 
+        {
+            Console.WriteLine("Que pena... Seu saldo foi insuficiente para finalizar a compra...");
+            Console.WriteLine("Saldo atual é R$" + saldo + " e o valor do produto é R$" + valorProduto);
+            Console.WriteLine("Faltou R$" + (conta * -1) + " para finalizar a compra.");
+        }
+
+        Console.ReadKey();
     }
 }
